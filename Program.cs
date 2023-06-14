c3d5c88bc8fbc2a700390f2b3b361276e6a079de
stitchees <54647336+stitchees@users.noreply.github.com>
2023-06-13 22:29:04 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,21 @@
+using byAnonymous.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connection = String.Empty;
+if (builder.Environment.IsDevelopment())
+{
+    builder.Configuration.AddEnvironmentVariables().AddJsonFile("appsettings.json");
+    connection = builder.Configuration.GetConnectionString("AZURE_SQL");
+}
+else
+{
+    connection = Environment.GetEnvironmentVariable("AZURE_SQL");
+}
+
+builder.Services.AddDbContext<MensajeDbContext>(options =>
+    options.UseSqlServer(connection));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -9,11 +26,8 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
 }
 
-app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
@@ -23,5 +37,21 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapGet("/Mensaje", (MensajeDbContext context) =>
+{
+    return context.Mensaje.ToList();
+})
+.WithName("GetMensajes");
+//dan error por eso estan comentadas si funciona se borran 
+//.WithOpenApi();
+
+app.MapPost("/Mensaje", (Mensaje mensaje, MensajeDbContext context) =>
+{
+    context.Add(mensaje);
+    context.SaveChanges();
+})
+.WithName("CreateMensaje");
+//.WithOpenApi();
 
 app.Run();
